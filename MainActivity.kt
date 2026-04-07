9cdbc2200f8892efc5331ca30ef00b60a994e50b
Mert Barutçu <barutcu.462@gmail.com>
2026-04-07 22:17:30 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast 
+import android.widget.Toast  
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
